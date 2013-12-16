a71f806c3c757d6042230749207ca2cee808792c
Mark <m@rkw.me>
2013-12-16 12:59:51 +0000

    [OE-3856] removed unnecessary statements

--- migrations/m131205_141328_table_versioning.php
+++ migrations/m131205_141328_table_versioning.php
@@ -639,39 +639,22 @@ CREATE TABLE `et_ophciglaucomaexamination_va_version` (
 	public function down()
 	{
 		$this->dropColumn('et_ophciglaucomaexamination_ant_seg','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_ant_seg_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_as','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_as_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_diagnosis','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_diagnosis_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_disc_files','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_disc_files_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_disc_info','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_disc_info_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_fam_hist','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_fam_hist_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_fup','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_fup_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_gonioscopy','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_gonioscopy_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_iop','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_iop_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_med_hist','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_med_hist_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_medications','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_medications_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_oc_hist','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_oc_hist_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_optic_disk','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_optic_disk_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_post_seg','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_post_seg_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_presc_meds','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_presc_meds_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_risks','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_risks_version','deleted');
 		$this->dropColumn('et_ophciglaucomaexamination_va','deleted');
-		$this->dropColumn('et_ophciglaucomaexamination_va_version','deleted');
 
 		$this->dropTable('et_ophciglaucomaexamination_ant_seg_version');
 		$this->dropTable('et_ophciglaucomaexamination_as_version');
