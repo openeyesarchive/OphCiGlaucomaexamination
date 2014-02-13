9889a3ac4b4434768acf211f21aaef494794f726
Mark <m@rkw.me>
2014-02-13 14:31:26 +0000

    soft deletion changes

--- models/ElementFamilyHistory.php
+++ models/ElementFamilyHistory.php
@@ -28,82 +28,82 @@
  */
 class ElementFamilyHistory extends BaseEventTypeElement {
 
-  /**
-   * Returns the static model of the specified AR class.
-   * @return ElementOperation the static model class
-   */
-  public static function model($className = __CLASS__) {
-    return parent::model($className);
-  }
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return ElementOperation the static model class
+	 */
+	public static function model($className = __CLASS__) {
+		return parent::model($className);
+	}
 
-  /**
-   * @return string the associated database table name
-   */
-  public function tableName() {
-    return 'et_ophciglaucomaexamination_fam_hist';
-  }
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName() {
+		return 'et_ophciglaucomaexamination_fam_hist';
+	}
 
-  /**
-   * @return array validation rules for model attributes.
-   */
-  public function rules() {
-    // NOTE: you should only define rules for those attributes that
-    // will receive user inputs.
-    return array(
-        array('event_id, comments', 'safe'),
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules() {
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+				array('event_id, comments', 'safe'),
 // The following rule is used by search().
 // Please remove those attributes that should not be searched.
-        array('id, event_id, comments', 'safe', 'on' => 'search'),
-    );
-  }
+				array('id, event_id, comments', 'safe', 'on' => 'search'),
+		);
+	}
 
-  /**
-   * @return array relational rules.
-   */
-  public function relations() {
-    // NOTE: you may need to adjust the relation name and the related
-    // class name for the relations automatically generated below.
-    return array(
-        'element_type' => array(self::HAS_ONE, 'ElementType', 'id', 'on' => "element_type.class_name='" . get_class($this) . "'"),
-        'eventType' => array(self::BELONGS_TO, 'EventType', 'event_type_id'),
-        'event' => array(self::BELONGS_TO, 'Event', 'event_id'),
-        'user' => array(self::BELONGS_TO, 'User', 'created_user_id'),
-        'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
-    );
-  }
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations() {
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'element_type' => array(self::HAS_ONE, 'ElementType', 'id', 'on' => "element_type.class_name='" . get_class($this) . "'"),
+			'eventType' => array(self::BELONGS_TO, 'EventType', 'event_type_id'),
+			'event' => array(self::BELONGS_TO, 'Event', 'event_id'),
+			'user' => array(self::BELONGS_TO, 'User', 'created_user_id'),
+			'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
+		);
+	}
 
-  /**
-   * @return array customized attribute labels (name=>label)
-   */
-  public function attributeLabels() {
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels() {
+		return array(
+			'comments' => 'Family',
+		);
+	}
 
-    return array(
-        'comments' => 'Family',
-    );
-  }
+	public function getAnaesthetist_list() {
+		$criteria = new CDbCriteria;
+		$criteria->compare('is_doctor', 1);
+		$criteria->order = 'first_name,last_name asc';
 
-  public function getAnaesthetist_list() {
-    $criteria = new CDbCriteria;
-    $criteria->compare('is_doctor', 1);
-    $criteria->order = 'first_name,last_name asc';
-    return User::model()->findAll($criteria);
-  }
+		return User::model()->active()->findAll($criteria);
+	}
 
-  /**
-   * Retrieves a list of models based on the current search/filter conditions.
-   * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-   */
-  public function search() {
-    // Warning: Please modify the following code to remove attributes that
-    // should not be searched.
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search() {
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
 
-    $criteria = new CDbCriteria;
+		$criteria = new CDbCriteria;
 
-    $criteria->compare('id', $this->id, true);
-    $criteria->compare('event_id', $this->event_id, true);
+		$criteria->compare('id', $this->id, true);
+		$criteria->compare('event_id', $this->event_id, true);
 
-    return new CActiveDataProvider(get_class($this), array(
-                'criteria' => $criteria,
-            ));
-  }
+		return new CActiveDataProvider(get_class($this), array(
+			'criteria' => $criteria,
+		));
+	}
 }
--- models/ElementPastMedicalHistory.php
+++ models/ElementPastMedicalHistory.php
@@ -16,8 +16,6 @@
  * @copyright Copyright (c) 2011-2012, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
-?>
-<?php
 
 /**
  * The followings are the available columns in table '':
@@ -29,83 +27,83 @@
  */
 class ElementPastMedicalHistory extends BaseEventTypeElement {
 
-  /**
-   * Returns the static model of the specified AR class.
-   * @return ElementOperation the static model class
-   */
-  public static function model($className = __CLASS__) {
-    return parent::model($className);
-  }
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return ElementOperation the static model class
+	 */
+	public static function model($className = __CLASS__) {
+		return parent::model($className);
+	}
 
-  /**
-   * @return string the associated database table name
-   */
-  public function tableName() {
-    return 'et_ophciglaucomaexamination_med_hist';
-  }
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName() {
+		return 'et_ophciglaucomaexamination_med_hist';
+	}
 
-  /**
-   * @return array validation rules for model attributes.
-   */
-  public function rules() {
-    // NOTE: you should only define rules for those attributes that
-    // will receive user inputs.
-    return array(
-        array('event_id, comments', 'safe'),
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules() {
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+				array('event_id, comments', 'safe'),
 // The following rule is used by search().
 // Please remove those attributes that should not be searched.
-        array('id, event_id, comments', 'safe', 'on' => 'search'),
-    );
-  }
+				array('id, event_id, comments', 'safe', 'on' => 'search'),
+		);
+	}
 
-  /**
-   * @return array relational rules.
-   */
-  public function relations() {
-    // NOTE: you may need to adjust the relation name and the related
-    // class name for the relations automatically generated below.
-    return array(
-        'element_type' => array(self::HAS_ONE, 'ElementType', 'id', 'on' => "element_type.class_name='" . get_class($this) . "'"),
-        'eventType' => array(self::BELONGS_TO, 'EventType', 'event_type_id'),
-        'event' => array(self::BELONGS_TO, 'Event', 'event_id'),
-        'user' => array(self::BELONGS_TO, 'User', 'created_user_id'),
-        'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
-    );
-  }
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations() {
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'element_type' => array(self::HAS_ONE, 'ElementType', 'id', 'on' => "element_type.class_name='" . get_class($this) . "'"),
+			'eventType' => array(self::BELONGS_TO, 'EventType', 'event_type_id'),
+			'event' => array(self::BELONGS_TO, 'Event', 'event_id'),
+			'user' => array(self::BELONGS_TO, 'User', 'created_user_id'),
+			'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
+		);
+	}
 
-  /**
-   * @return array customized attribute labels (name=>label)
-   */
-  public function attributeLabels() {
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels() {
 
-    return array(
-        'comments' => 'Medical',
-    );
-  }
+		return array(
+			'comments' => 'Medical',
+		);
+	}
 
-  public function getAnaesthetist_list() {
-    $criteria = new CDbCriteria;
-    $criteria->compare('is_doctor', 1);
-    $criteria->order = 'first_name,last_name asc';
-    return User::model()->findAll($criteria);
-  }
+	public function getAnaesthetist_list() {
+		$criteria = new CDbCriteria;
+		$criteria->compare('is_doctor', 1);
+		$criteria->order = 'first_name,last_name asc';
 
-  /**
-   * Retrieves a list of models based on the current search/filter conditions.
-   * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-   */
-  public function search() {
-    // Warning: Please modify the following code to remove attributes that
-    // should not be searched.
+		return User::model()->active()->findAll($criteria);
+	}
 
-    $criteria = new CDbCriteria;
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search() {
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
 
-    $criteria->compare('id', $this->id, true);
-    $criteria->compare('event_id', $this->event_id, true);
+		$criteria = new CDbCriteria;
 
-    return new CActiveDataProvider(get_class($this), array(
-                'criteria' => $criteria,
-            ));
-  }
+		$criteria->compare('id', $this->id, true);
+		$criteria->compare('event_id', $this->event_id, true);
 
+		return new CActiveDataProvider(get_class($this), array(
+			'criteria' => $criteria,
+		));
+	}
 }
