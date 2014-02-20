7e58472142eb124498e049f6cccbc88cd6a7a442
Mark <m@rkw.me>
2014-02-20 12:56:06 +0000

    removed spurious whitespace.

--- models/EyeDrawBase.php
+++ models/EyeDrawBase.php
@@ -16,8 +16,6 @@
  * @copyright Copyright (c) 2011-2012, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
-?>
- <?php
 
 /*
  * Conwenience class for all glaucoma elements.
